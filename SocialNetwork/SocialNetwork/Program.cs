bfa38f1d92eb05663ac2ad90e815409b01b05c42
Eazhilarasi <Eazhilarasi.Manivannan@ucdconnect.ie>
2016-12-13 18:51:00 +0000

    Total number of members in social network

--- SocialNetwork/SocialNetwork/Program.cs
+++ SocialNetwork/SocialNetwork/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,25 @@ namespace SocialNetwork
     {
         static void Main(string[] args)
         {
+            Dictionary<String, string> friendsMapping = new Dictionary<string, string>();
             Console.WriteLine("Hello");
-            string[] lines = System.IO.File.ReadAllLines("SocialNetwork.txt");
+            string[] friends = System.IO.File.ReadAllLines(@"Source\SocialNetwork.txt");
+            foreach(string pair in friends)
+            {
+                string[] members = pair.Split(',');
+                if(friendsMapping.ContainsKey(members[0]))
+                {
+                    string value = friendsMapping[members[0]];
+                    value = value + ',' + members[1];
+                    friendsMapping[members[0]] = value;
+                }
+                else
+                {
+                    friendsMapping[members[0]] = members[1];
+                }
+            }
+
+            Console.WriteLine("Total number of members in Social network is" + friendsMapping.Count);
         }
     }
 }
